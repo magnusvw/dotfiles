10c446a900b7aaf2b673e897c0686448148a65d7
Magnus von Wachenfeldt <magnusvw@gmail.com>
2016-06-14 01:42:55 +0200

    lel

---
+++ setup.sh
@@ -0,0 +1,9 @@
+mkdir -p ~/.config
+ln -sf $PWD/vim/vimrc ~/.vimrc
+ln -sf $PWD/awesome ~/.config/awesome
+ln -sf $PWD/X11/Xresources ~/.Xresources
+ln -sf $PWD/X11/xinitrc ~/.xinitrc
+ln -sf $PWD/shell/oh-my-zsh ~/.oh-my-zsh
+ln -sf $PWD/shell/zshrc ~/.zshrc
+ln -sf $PWD/shell/profile ~/.profile
+ln -sf $PWD/git/gitconfig ~/.gitconfig
